c120908f1294ba6aa32a7da589338003c5486a2d
S, Praveen (Cognizant) <praveen.s.contractor@pepsico.com>
2024-07-25 23:34:08 +0530

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by S, Praveen (Cognizant) on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
